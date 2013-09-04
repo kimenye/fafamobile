eb7ea455f12de355b534ad266a5eb3bc8cca9e33
Trevor Kimenye <kimenye@gmail.com>
2013-09-04 15:27:32 +0300

    added home body panel

--- app/view/HomeBody.js
+++ app/view/HomeBody.js
@@ -3,7 +3,7 @@ Ext.define('FaFa.view.HomeBody', {
 	xtype: 'homebody',
 	config: {
         // html: "<h4>What is FAFA models about</h4><p>The first thing you should understand about the cloud is that it is not a physical thing. The cloud is a network of servers, and each server has a different function. Some servers use computing power to run applications</p>",
-		// styleHtmlContent: true
+		styleHtmlContent: true,
 		items: [{
 		        xtype : 'button',
 		        cls   : 'modus-button info',
--- app/view/Main.js
+++ app/view/Main.js
@@ -1,43 +1,20 @@
 Ext.define('FaFa.view.Main', {
-    extend: 'Ext.tab.Panel',
+    extend: 'Ext.Panel',
     xtype: 'main',
     requires: [
-        'Ext.TitleBar',
-        'Ext.Video',
-		'FaFa.view.HeaderToolbar'
+		'FaFa.view.HeaderToolbar',
+		'FaFa.view.HomeBody'
     ],
     config: {
-        // tabBarPosition: 'bottom',
-
         items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: [{
-                    docked: 'top',
-                    xtype: 'headertoolbar',
-                    title: ''
-                },
-				{
-					xtype : 'button',
-			        cls   : 'modus-button info',
-			        text  : 'What is FaFa Models?'
-			    },
-			    {
-			        xtype : 'button',
-			        cls   : 'modus-button info',
-			        text  : 'I want to join'
-			    },
-			    {
-			        xtype : 'button',
-			        cls   : 'modus-button info',
-			        text  : 'Take a Tour'
-			    }]
-            }
-        ]
+			{
+                docked: 'top',
+                xtype: 'headertoolbar',
+                title: ''
+            },
+			{
+				xtype : 'homebody'
+		    }
+		]
     }
 });
