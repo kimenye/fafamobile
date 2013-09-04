0d8a0b3eadada74bd1dba9f3b48dbae521b9918d
Trevor Kimenye <kimenye@gmail.com>
2013-09-04 12:25:12 +0300

    added the bottom home panel

--- app/view/Main.js
+++ app/view/Main.js
@@ -2,7 +2,8 @@ Ext.define('FaFa.view.Main', {
     extend: 'Ext.Panel',
     xtype: 'main',
     requires: [
-        'FaFa.view.HeaderToolbar'
+        'FaFa.view.HeaderToolbar',
+		'FaFa.view.HomeBody'
     ],
     config: {
         items: [
@@ -10,8 +11,7 @@ Ext.define('FaFa.view.Main', {
                 xtype: 'headertoolbar'
             }, 
 			{				
-                html: "<h4>What is FAFA models about</h4><p>The first thing you should understand about the cloud is that it is not a physical thing. The cloud is a network of servers, and each server has a different function. Some servers use computing power to run applications</p>",
-				styleHtmlContent: true
+                xtype: 'homebody'
             }
         ]
     }
