521106a2573e4dcac42009daeca6183ef23a2f9a
Trevor Kimenye <kimenye@gmail.com>
2013-09-14 07:01:45 +0300

    added carousel view

--- app/view/About.js
+++ app/view/About.js
@@ -1,17 +1,22 @@
 Ext.define('FaFa.view.About', {
-	extend: 'Ext.Panel',
+	extend: 'Ext.Carousel',
 	xtype: 'about-view',
 	config: {
-		cls: 'about-pane',
-		styleHtmlContent: true,
+		// cls: 'about-pane',
+		// styleHtmlContent: true,
 		items: [
 			{
+				config : {
+					cls: 'about-pane',
+					styleHtmlContent: true
+				},
 				html: '<div class="info-pane"><h3>What is FAFA models about</h3><p>FAFA -Festival of African Fashion and Arts- 2010. FAFA brings to light the creation of a movement within African designers and artists which embraces and embodies ethical fashion and the values it upholds. Through increasing the capacity and well-being of the people and communities, creativity, design and fashion can play an active role in poverty reduction, the development of sustainable livelihoods and the reduction of the harmful effects on the environment</p></div>'			
 			},
 			{
-				xtype: 'button',
-				text: 'Tour',
-				cls: 'modus-button primary signup'
+				config : {
+					cls: 'about-pane'
+				},
+				html: '<div class="info-pane"><h3>How do you join?</h3><p>FAFA -Festival of African Fashion and Arts- 2010. FAFA brings to light the creation of a movement within African designers and artists which embraces and embodies ethical fashion and the values it upholds. Through increasing the capacity and well-being of the people and communities, creativity, design and fashion can play an active role in poverty reduction, the development of sustainable livelihoods and the reduction of the harmful effects on the environment</p></div>'	
 			}											
 		]					
 	}	
