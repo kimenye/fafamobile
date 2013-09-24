eefa242079dca4abc79701d8016eeee920891aeb
Trevor Kimenye <kimenye@gmail.com>
2013-09-24 08:35:59 +0300

    Removed scrollable from TourCard

--- app/view/TourCard.js
+++ app/view/TourCard.js
@@ -8,7 +8,7 @@ Ext.define('FaFa.view.TourCard', {
 		items: [
 			{
 				styleHtmlContent: true,
-				scrollable: true,
+				// scrollable: true,
 				html: '<h1 style="font-size:16px;"><b>About Roses</b></h1><p>The leaves are borne alternately on the stem. In most species they are 5 to 15 centimetres (2.0 to 5.9 in) long, pinnate, with leaflets and basal stipules; the leaflets usually have a serrated margin, and often a few small prickles on the underside of the stem. Most roses are deciduous but a few (particularly from South east Asia) are evergreen or nearly so.</p>'
 			}
 		]
